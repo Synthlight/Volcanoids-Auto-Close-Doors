0ddef726100e5ac8540580348c400439a13e61e1
Gregory Brutsche <gregory@mail.brutsches.local>
2020-02-25 01:44:46 -0500

    Initial commit.

---
+++ AutoCloseDoorsMod.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Auto_Close_Doors {
+    [BepInPlugin(UUID, "Auto-Close-Doors Mod", "1.0.0.0")]
+    public class AutoCloseDoorsMod : BaseUnityPlugin {
+        private const string UUID = "com.autoCloseDoors";
+        private static ManualLogSource logSource;
+
+        public void Awake() {
+            logSource = Logger;
+
+            Log(LogLevel.Info, "Auto-Close-Doors loaded.");
+
+            var harmony = new Harmony(UUID);
+            harmony.PatchAll();
+
+            foreach (var patchedMethod in harmony.GetPatchedMethods()) {
+                Log(LogLevel.Info, $"Patched: {patchedMethod.DeclaringType?.FullName}:{patchedMethod}");
+            }
+        }
+
+        public static void Log(LogLevel level, string msg) {
+            logSource.Log(level, msg);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Volcanoids Auto-Close-Doors Mod
+This is a mod to automatically close doors players open, automatically, a few seconds later.
+
+# Requirements
+- Volcanoids obviously.
+- BepInEx: https://github.com/BepInEx/BepInEx (You want the x64 version.)
+
+# Installation
+- Extract BepInEx to the root Volcanoids folder so `winhttp.dll` is in the same folder as `Volcanoids.exe`.
+- Extract this mod so `Auto-Sort.dll` is placed like this: `Volcanoids\BepInEx\plugins\Auto-Close-Doors.dll`.
+
+# Un-installation
+BepInEx uses `winhttp.dll` as an injector/loader. Renaming or deleting this file is enough to disable both my mod and the loader.
