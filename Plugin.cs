cc2740c0ddcd85cc856ee4f462a80cefdf923b4e
Gregory Brutsche <gregory@mail.brutsches.local>
2020-12-28 11:37:50 -0500

    Redo mod for built-in mod loader. (Bump version to 1.2.)

---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Auto_Close_Doors {
+    [UsedImplicitly]
+    public class Plugin : GameMod {
+        private readonly GUID copperDoorGuid = GUID.Parse("2f37c2f7701fb8b44abefdcd03681b8b");
+
+        public override void Load() {
+            Debug.Log("Auto-Close-Doors loaded.");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public override void Unload() {
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode) {
+            if (scene.name != "Island") return;
+
+            var doorItemDef = GameResources.Instance.Items.First(item => item.AssetId == copperDoorGuid);
+
+            // Add to door prefabs.
+            if (doorItemDef?.Prefabs != null) {
+                foreach (var prefab in doorItemDef.Prefabs) {
+                    if (!prefab.HasComponent<AutoCloseComponent>() && prefab.HasComponent<DoorUseAnimation>()) {
+                        prefab.AddComponent<AutoCloseComponent>();
+                    }
+                }
+            }
+
+            // Add to existing door objects.
+            foreach (var doorAnim in Resources.FindObjectsOfTypeAll<DoorUseAnimation>()) {
+                if (!doorAnim.gameObject.HasComponent<AutoCloseComponent>()) {
+                    doorAnim.gameObject.AddComponent<AutoCloseComponent>();
+                }
+            }
+        }
+
+        [UsedImplicitly]
+        public class AutoCloseComponent : MonoBehaviour {
+            private bool canClose;
+            private bool waitingForClose;
+
+            public void CloseDoor() {
+                if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
+
+                doorUseAnimation.Open(false, false);
+                waitingForClose = false;
+            }
+
+            [UsedImplicitly]
+            public void Update() {
+                if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
+
+                // CurrentState true means door is open or it started closing (but not closed yet).
+                // TargetState true means door is opening or open.
+
+                // Wait for open, not animating, and not waiting for a delayed close.
+                if (doorUseAnimation.TargetState && doorUseAnimation.IsAnimating && !waitingForClose) {
+                    canClose = true;
+                }
+
+                // Then send close.
+                if (doorUseAnimation.TargetState && !doorUseAnimation.IsAnimating && canClose) {
+                    canClose        = false;
+                    waitingForClose = true; // So we don't keep sending this till it's re-opened.
+                    Invoke(nameof(CloseDoor), 2f);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 ﻿# Volcanoids Auto-Close-Doors Mod
-This is a mod to automatically close doors players open, automatically, a few seconds later.
+This is a mod to automatically closes doors players open, automatically, two seconds later.
 
 # Requirements
 - Volcanoids obviously.
-- BepInEx: https://github.com/BepInEx/BepInEx (You want the x64 version.)
 
 # Installation
-- Extract BepInEx to the root Volcanoids folder so `winhttp.dll` is in the same folder as `Volcanoids.exe`.
-- Extract this mod so `Auto-Sort.dll` is placed like this: `Volcanoids\BepInEx\plugins\Auto-Close-Doors.dll`.
+- Extract the release to the Volcanoids mod folder (`%LOCALAPPDATA%Low\Volcanoid\Volcanoids\Mods`) so `Auto-Close-Doors.dll` is placed like this: `%LOCALAPPDATA%Low\Volcanoid\Volcanoids\Mods\Auto-Close-Doors.dll`.
 
 # Un-installation
-BepInEx uses `winhttp.dll` as an injector/loader. Renaming or deleting this file is enough to disable both my mod and the loader.
+- Just remove the dll from `%LOCALAPPDATA%Low\Volcanoid\Volcanoids\Mods`.
