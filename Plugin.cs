230dff2af74899795c2dcd806310cbf457743539
Gregory Brutsche <gregory@mail.brutsches.local>
2021-10-13 15:42:09 -0400

    Final touches for the workshop update.

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,18 @@
-﻿using System.Linq;
-using Base_Mod;
+﻿using Base_Mod;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Auto_Close_Doors {
     [UsedImplicitly]
     public class Plugin : BaseGameMod {
-        protected override      string ModName => "Auto-Close-Doors";
-        private static readonly GUID   COPPER_DOOR_GUID = GUID.Parse("2f37c2f7701fb8b44abefdcd03681b8b");
+        private static readonly GUID COPPER_DOOR_GUID = GUID.Parse("2f37c2f7701fb8b44abefdcd03681b8b");
 
         // Add to door prefabs.
         public override void OnInitData() {
-            var doorItemDef = GameResources.Instance.Items.First(item => item.AssetId == COPPER_DOOR_GUID);
-
-            if (doorItemDef.Prefabs != null) {
-                foreach (var prefab in doorItemDef.Prefabs) {
-                    if (!prefab.HasComponent<AutoCloseComponent>() && prefab.HasComponent<DoorUseAnimation>()) {
-                        prefab.AddComponent<AutoCloseComponent>();
-                    }
+            foreach (var prefab in Lib.GetItemDefinitionWithComponent<DoorUseAnimation>(COPPER_DOOR_GUID)) {
+                if (!prefab.HasComponent<AutoCloseComponent>() && prefab.HasComponent<DoorUseAnimation>()) {
+                    prefab.AddComponent<AutoCloseComponent>();
                 }
             }
 
@@ -25,14 +20,14 @@ namespace Auto_Close_Doors {
         }
 
         // Add to existing door objects.
-        public override void OnGameLoaded() {
+        public override void OnGameLoaded(Scene scene) {
             foreach (var doorAnim in Resources.FindObjectsOfTypeAll<DoorUseAnimation>()) {
                 if (!doorAnim.gameObject.HasComponent<AutoCloseComponent>()) {
                     doorAnim.gameObject.AddComponent<AutoCloseComponent>();
                 }
             }
 
-            base.OnGameLoaded();
+            base.OnGameLoaded(scene);
         }
     }
 }
