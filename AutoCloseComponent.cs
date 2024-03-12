cdbaec4b44b0dd0314efa3b76b99e541ddbd9226
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:22:38 -0500

    Update for Volc 1.31.

--- AutoCloseComponent.cs
+++ AutoCloseComponent.cs
@@ -1,36 +1,36 @@
 ﻿using JetBrains.Annotations;
 using UnityEngine;
 
-namespace Auto_Close_Doors {
-    public class AutoCloseComponent : MonoBehaviour {
-        private bool canClose;
-        private bool waitingForClose;
+namespace Auto_Close_Doors;
 
-        public void CloseDoor() {
-            if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
+public class AutoCloseComponent : MonoBehaviour {
+    private bool canClose;
+    private bool waitingForClose;
 
-            doorUseAnimation.Open(false, false);
-            waitingForClose = false;
+    public void CloseDoor() {
+        if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
+
+        doorUseAnimation.Open(false, false);
+        waitingForClose = false;
+    }
+
+    [UsedImplicitly]
+    public void Update() {
+        if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
+
+        // CurrentState true means door is open or it started closing (but not closed yet).
+        // TargetState true means door is opening or open.
+
+        // Wait for open, not animating, and not waiting for a delayed close.
+        if (doorUseAnimation.TargetState && doorUseAnimation.IsAnimating && !waitingForClose) {
+            canClose = true;
         }
 
-        [UsedImplicitly]
-        public void Update() {
-            if (!gameObject.TryGetComponentSafe(out DoorUseAnimation doorUseAnimation)) return;
-
-            // CurrentState true means door is open or it started closing (but not closed yet).
-            // TargetState true means door is opening or open.
-
-            // Wait for open, not animating, and not waiting for a delayed close.
-            if (doorUseAnimation.TargetState && doorUseAnimation.IsAnimating && !waitingForClose) {
-                canClose = true;
-            }
-
-            // Then send close.
-            if (doorUseAnimation.TargetState && !doorUseAnimation.IsAnimating && canClose) {
-                canClose        = false;
-                waitingForClose = true; // So we don't keep sending this till it's re-opened.
-                Invoke(nameof(CloseDoor), 2f);
-            }
+        // Then send close.
+        if (doorUseAnimation.TargetState && !doorUseAnimation.IsAnimating && canClose) {
+            canClose        = false;
+            waitingForClose = true; // So we don't keep sending this till it's re-opened.
+            Invoke(nameof(CloseDoor), 2f);
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -3,31 +3,31 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Auto_Close_Doors {
-    [UsedImplicitly]
-    public class Plugin : BaseGameMod {
-        private static readonly GUID COPPER_DOOR_GUID = GUID.Parse("2f37c2f7701fb8b44abefdcd03681b8b");
+namespace Auto_Close_Doors;
 
-        // Add to door prefabs.
-        public override void OnInitData() {
-            foreach (var prefab in Lib.GetItemDefinitionWithComponent<DoorUseAnimation>(COPPER_DOOR_GUID)) {
-                if (!prefab.HasComponent<AutoCloseComponent>() && prefab.HasComponent<DoorUseAnimation>()) {
-                    prefab.AddComponent<AutoCloseComponent>();
-                }
+[UsedImplicitly]
+public class Plugin : BaseGameMod {
+    private static readonly GUID COPPER_DOOR_GUID = GUID.Parse("2f37c2f7701fb8b44abefdcd03681b8b");
+
+    // Add to door prefabs.
+    public override void OnInitData() {
+        foreach (var prefab in Lib.GetItemDefinitionWithComponent<DoorUseAnimation>(COPPER_DOOR_GUID)) {
+            if (!prefab.HasComponent<AutoCloseComponent>() && prefab.HasComponent<DoorUseAnimation>()) {
+                prefab.AddComponent<AutoCloseComponent>();
             }
-
-            base.OnInitData();
         }
 
-        // Add to existing door objects.
-        public override void OnGameLoaded(Scene scene) {
-            foreach (var doorAnim in Resources.FindObjectsOfTypeAll<DoorUseAnimation>()) {
-                if (!doorAnim.gameObject.HasComponent<AutoCloseComponent>()) {
-                    doorAnim.gameObject.AddComponent<AutoCloseComponent>();
-                }
-            }
+        base.OnInitData();
+    }
 
-            base.OnGameLoaded(scene);
+    // Add to existing door objects.
+    public override void OnGameLoaded(Scene scene) {
+        foreach (var doorAnim in Resources.FindObjectsOfTypeAll<DoorUseAnimation>()) {
+            if (!doorAnim.gameObject.HasComponent<AutoCloseComponent>()) {
+                doorAnim.gameObject.AddComponent<AutoCloseComponent>();
+            }
         }
+
+        base.OnGameLoaded(scene);
     }
 }
